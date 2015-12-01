5fd4311dd662cfca3f0c0c0a359492d1c40d58ad
Chris Olstrom <chris@olstrom.com>
2015-12-01 09:43:50 -0800

    Add function to find and apply all policies for a user or group

---
+++ functions/apply-policies.fish
@@ -0,0 +1,6 @@
+function apply-policies --argument-names type target
+  print-status iam/apply-policies "$type $target"
+  for policy in (find-policies $type $target)
+    apply-policy $type $target $policy
+  end
+end
