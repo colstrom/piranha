752306c1340bd0ed2ec713ab8aaf447f2bc3ad8f
Chris Olstrom <chris@olstrom.com>
2015-12-01 09:43:31 -0800

    Add function to apply a policy to a user or group

---
+++ functions/apply-policy.fish
@@ -0,0 +1,4 @@
+function apply-policy --argument-names type target policy
+  print-status iam/apply-policy "$type $target $policy"
+  aws iam put-$type-policy --$type-name $target --policy-name $policy --policy-document file://iam/$type-policy/$target/$policy.json
+end
