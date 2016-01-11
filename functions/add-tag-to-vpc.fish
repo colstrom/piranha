0b157a2c2eeb0aff53979c64d651fb414e212989
Chris Olstrom <chris@olstrom.com>
2016-01-11 15:11:40 -0800

    Add function to add a tag to a VPC

---
+++ functions/add-tag-to-vpc.fish
@@ -0,0 +1,11 @@
+function add-tag-to-vpc --argument-names resource key value
+    print-status vpc/add-tag "$argv"
+    for argument in resource key value
+        if test -z "$$argument"
+            print-status usage "add-tag-to-vpc <vpc-id> <key> <value>"
+            return -1
+        end
+    end
+
+    and aws ec2 create-tags --resources $resource --tags Key=$key,Value=$value
+end
