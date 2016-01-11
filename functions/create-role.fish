6313be77f5843c08196b9652b3b10562730effd3
Chris Olstrom <chris@olstrom.com>
2016-01-11 15:05:06 -0800

    Add function to create role

---
+++ functions/create-role.fish
@@ -0,0 +1,17 @@
+function create-role --argument-names name
+    print-status iam/create-role $name
+
+    if test -z "$name"
+        print-status usage 'create-role <name>'
+        return -1
+    end
+
+    set --local role iam/roles/$name
+    set --local policy assume-role-policy.json
+
+    if has $policy $role
+        aws iam create-role --role-name $name --assume-role-policy-document file://$role/$policy
+    else
+        print-status error "Expected to find policy at $role/$policy"
+    end
+end
