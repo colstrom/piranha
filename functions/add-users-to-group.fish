d8864b3a77f2b9f7bc1671b563aa7c54dbd7df5d
Chris Olstrom <chris@olstrom.com>
2016-01-11 14:59:22 -0800

    Remove add-users-to-group

--- functions/add-users-to-group.fish
+++
@@ -1,7 +0,0 @@
-function add-users-to-group --argument-names group
-    print-status iam/add-users-to-group $group
-    group-exists $group
-    and for user in (list-users-in-group $group)
-        add-user-to-group $user $group
-    end
-end
