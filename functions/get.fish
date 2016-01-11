d72619b7ee5d8898ebe3bbdde5ad5df2f5034430
Chris Olstrom <chris@olstrom.com>
2016-01-11 15:01:38 -0800

    Add get function
    `get` treats `$PWD` as a database of sorts.

---
+++ functions/get.fish
@@ -0,0 +1,17 @@
+function get --argument-names key
+    if test -z "$key"
+        print-status usage 'get <key>'
+        return -1
+    end
+
+    if test -e $key
+        switch (describe $key)
+            case 'directory'
+                find $key -mindepth 1 -maxdepth 1 -exec basename '{}' \; | grep -v '^\.'
+            case '*'
+                cat $key
+        end
+    else
+        return 1
+    end
+end
