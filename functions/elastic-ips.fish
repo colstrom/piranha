a789f0d979df9e273f674c8ab7aca6011e6604ed
Chris Olstrom <chris@olstrom.com>
2016-01-15 15:19:35 -0800

    Add function to find Elastic IPs

---
+++ functions/elastic-ips.fish
@@ -0,0 +1,22 @@
+function elastic-ips --argument-names resource
+    set --local resource_type (echo "$resource" | cut -f 1 -d '-')
+    if test "$resource_type" = 'eni'
+        aws ec2 describe-network-interfaces --filters=Name=network-interface-id,Values=$resource | jq --raw-output .NetworkInterfaces[].Association.PublicIp
+    else
+        set --local filter
+        switch "$resource_type"
+            case 'i'
+                set filter instance-id
+            case 'eipassoc'
+                set filter association-id
+            case 'eipalloc'
+                set filter allocation-id
+        end
+
+        if test ! -z "$filter"
+            aws ec2 describe-addresses --filters Name=$filter,Values=$resource | jq --raw-output .Addresses[].PublicIp
+        else
+            aws ec2 describe-addresses | jq --raw-output .Addresses[].PublicIp
+        end
+    end
+end
