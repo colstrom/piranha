6df459e53f0a91a6aedf43b8a3ca7d088e107fda
Chris Olstrom <chris@olstrom.com>
2016-02-01 15:13:57 -0800

    Add support for polices to update-buckets

--- functions/update-buckets.fish
+++ functions/update-buckets.fish
@@ -2,5 +2,8 @@ function update-buckets
     for bucket in (get s3/buckets)
         bucket-exists $bucket
         or create-bucket $bucket
+        and for policy in (get s3/buckets/$bucket/policies)
+            apply-policy s3/buckets/$bucket/policies/$policy
+        end
     end
 end
