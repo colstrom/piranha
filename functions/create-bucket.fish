e000db7445695688469c9522a274e15f4c44434c
Chris Olstrom <chris@olstrom.com>
2016-02-03 11:46:37 -0800

    Add support for bucket ACLs

--- functions/create-bucket.fish
+++ functions/create-bucket.fish
@@ -5,5 +5,8 @@ function create-bucket --argument-names name
     has region s3/buckets/$name
     and set -- options $options --create-bucket-configuration LocationConstraint=(get s3/buckets/$name/region)
 
+    has acl s3/buckets/$name
+    and set -- options $options --acl (get s3/buckets/$name/acl)
+
     aws s3api create-bucket --bucket $name $options
 end
