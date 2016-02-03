d0c866f7c94c44668a606757070e0b850bfb50c6
Chris Olstrom <chris@olstrom.com>
2016-02-03 11:18:56 -0800

    Add support for bucket regions

--- functions/create-bucket.fish
+++ functions/create-bucket.fish
@@ -1,4 +1,9 @@
 function create-bucket --argument-names name
     print-status s3/create-bucket $name
-    aws s3api create-bucket --bucket $name
+    set --local options
+
+    has region s3/buckets/$name
+    and set -- options $options --create-bucket-configuration LocationConstraint=(get s3/buckets/$name/region)
+
+    aws s3api create-bucket --bucket $name $options
 end
