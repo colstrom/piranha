8c50e7e59c3bc4159204cae204a365a73a4750c0
Chris Olstrom <chris@olstrom.com>
2016-02-03 17:50:10 -0800

    BUGFIX: has should exit if invalid arguments are passed

--- functions/has.fish
+++ functions/has.fish
@@ -1,6 +1,7 @@
 function has --argument-names key resource
     if test -z "$key" -o -z "$resource"
         print-status usage 'has <key> <resource>'
+        return (false)
     end
 
     contains $key (get $resource)
