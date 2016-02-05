613ec06afba5d969edec13d5b98cd24f434cee14
Chris Olstrom <chris@olstrom.com>
2016-02-04 17:35:03 -0800

    Add function to look up the ARN for a managed policy

---
+++ functions/lookup-policy.fish
@@ -0,0 +1,8 @@
+function lookup-policy --argument name
+    if test -z "$name"
+        stderr "usage: lookup-policy <name>"
+        return (false)
+    end
+
+    aws iam list-policies | jq --raw-output ".Policies[] | select(.PolicyName == \"$name\") .Arn"
+end
