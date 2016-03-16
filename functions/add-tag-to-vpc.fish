c2ce8a223fb621ef7390f556ad370520a98906d9
Chris Olstrom <chris@olstrom.com>
2016-03-16 16:54:24 -0700

    Replace loop over arguments with empty

--- functions/add-tag-to-vpc.fish
+++ functions/add-tag-to-vpc.fish
@@ -1,10 +1,8 @@
 function add-tag-to-vpc --argument-names resource key value
     print-status vpc/add-tag "$argv"
-    for argument in resource key value
-        if empty "$$argument"
-            print-status usage "add-tag-to-vpc <vpc-id> <key> <value>"
-            return -1
-        end
+    if empty "$resource" "$key" "$value"
+        print-status usage "add-tag-to-vpc <vpc-id> <key> <value>"
+        return -1
     end
 
     and aws ec2 create-tags --resources $resource --tags Key=$key,Value=$value
