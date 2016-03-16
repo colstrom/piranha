29e4c473e2cadbab296adec244a7e706f60dbc46
Chris Olstrom <chris@olstrom.com>
2016-03-16 16:52:30 -0700

    Replace test -z with empty in add-tag-to-vpc

--- functions/add-tag-to-vpc.fish
+++ functions/add-tag-to-vpc.fish
@@ -1,7 +1,7 @@
 function add-tag-to-vpc --argument-names resource key value
     print-status vpc/add-tag "$argv"
     for argument in resource key value
-        if test -z "$$argument"
+        if empty "$$argument"
             print-status usage "add-tag-to-vpc <vpc-id> <key> <value>"
             return -1
         end
