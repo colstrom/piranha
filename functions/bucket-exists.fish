48fdb492403ee5d5e15f95488261c7be8f29d1f1
Chris Olstrom <chris@olstrom.com>
2016-03-16 22:00:18 -0700

    Prefer --argument to --argument-names

--- functions/bucket-exists.fish
+++ functions/bucket-exists.fish
@@ -1,4 +1,4 @@
-function bucket-exists --argument-names name
+function bucket-exists --argument name
     print-status s3/bucket-exists $name
     aws s3api list-buckets | jq -e ".Buckets[] | select(.Name == \"$name\")" > /dev/null ^ /dev/null
 end
