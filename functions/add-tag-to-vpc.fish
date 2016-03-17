348dd9465664ad94d364f64792afdf7a52400d79
Chris Olstrom <chris@olstrom.com>
2016-03-16 21:58:17 -0700

    Prefer --argument to --argument-names

--- functions/add-tag-to-vpc.fish
+++ functions/add-tag-to-vpc.fish
@@ -1,4 +1,4 @@
-function add-tag-to-vpc --argument-names resource key value
+function add-tag-to-vpc --argument resource key value
     print-status vpc/add-tag "$argv"
     if empty "$resource" "$key" "$value"
         usage add-tag-to-vpc '<vpc-id>' '<key>' '<value>'
