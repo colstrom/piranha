ab998c5ee4a1c131406dbe131784edaa02061ab8
Chris Olstrom <chris@olstrom.com>
2016-03-17 08:15:19 -0700

    Add usage to group-exists

--- functions/group-exists.fish
+++ functions/group-exists.fish
@@ -1,4 +1,9 @@
 function group-exists --argument name
+    if empty "$name"
+        usage group-exists '<name>'
+        return (false)
+    end
+
     print-status iam/group-exists $name
     aws iam get-group --group-name $name > /dev/null ^ /dev/null
 end
