d27253f6b722444becb448a83047476c6c3e819a
Chris Olstrom <chris@olstrom.com>
2016-03-17 10:55:38 -0700

    Add function to delete a user

---
+++ functions/delete-user.fish
@@ -0,0 +1,17 @@
+function delete-user --argument user
+    if empty "$user"
+        usage delete-user '<user>'
+        return (false)
+    end
+
+    if contains "$user" (actual-users)
+        for group in (user-in-groups "$user")
+            delete-from-group "$group" "$user"
+        end
+
+        revoke-access-keys "$user"
+        unset-password "$user"
+        deactivate-mfa "$user"
+        aws iam delete-user --user-name "$user"
+    end
+end
