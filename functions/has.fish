5a6dca20c18ff03efd5138f92fb510e2f70688f9
Chris Olstrom <chris@olstrom.com>
2016-03-16 21:53:15 -0700

    Replace print-status with usage in has

--- functions/has.fish
+++ functions/has.fish
@@ -1,6 +1,6 @@
 function has --argument-names key resource
     if empty "$key" "$resource"
-        print-status usage 'has <key> <resource>'
+        usage has '<key>' '<resource>'
         return (false)
     end
 
