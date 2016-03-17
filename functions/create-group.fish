7e4e67718c5005f4d5e03fe596d2bf18dff128de
Chris Olstrom <chris@olstrom.com>
2016-03-17 08:14:57 -0700

    Add usage to create-group

--- functions/create-group.fish
+++ functions/create-group.fish
@@ -1,4 +1,9 @@
 function create-group --argument name
+    if empty "$name"
+        usage create-group '<name>'
+        return (false)
+    end
+
     print-status iam/create-group $name
     aws iam create-group --group-name $name
 end
