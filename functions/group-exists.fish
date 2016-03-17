21e21177e56de43fe58b1dfa64427cfa8a9c67d2
Chris Olstrom <chris@olstrom.com>
2016-03-16 22:00:52 -0700

    Prefer --argument to --argument-names

--- functions/group-exists.fish
+++ functions/group-exists.fish
@@ -1,4 +1,4 @@
-function group-exists --argument-names name
+function group-exists --argument name
     print-status iam/group-exists $name
     aws iam get-group --group-name $name > /dev/null ^ /dev/null
 end
