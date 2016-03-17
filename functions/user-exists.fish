514352a3fa9feff98dfccb5ea8f431b14e0d6e12
Chris Olstrom <chris@olstrom.com>
2016-03-17 08:15:59 -0700

    Add usage to user-exists

--- functions/user-exists.fish
+++ functions/user-exists.fish
@@ -1,4 +1,9 @@
 function user-exists --argument name
+    if empty "$name"
+        usage user-exists '<name>'
+        return (false)
+    end
+
     print-status iam/user-exists $name
     aws iam get-user --user-name $name > /dev/null ^ /dev/null
 end
