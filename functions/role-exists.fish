665b923680d5a841ac38dbf66e4034cb3ae064c4
Chris Olstrom <chris@olstrom.com>
2016-03-17 08:15:34 -0700

    Add usage to role-exists

--- functions/role-exists.fish
+++ functions/role-exists.fish
@@ -1,4 +1,9 @@
 function role-exists --argument name
+    if empty "$name"
+        usage role-exists '<name>'
+        return (false)
+    end
+
     print-status iam/role-exists $name
     aws iam get-role --role-name $name > /dev/null ^ /dev/null
 end
