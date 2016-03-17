8e175a1f2eb6a19956a5b29f25baea626fe37964
Chris Olstrom <chris@olstrom.com>
2016-03-17 12:45:53 -0700

    Remove iam prefix from update-groups

--- functions/update-groups.fish
+++ functions/update-groups.fish
@@ -1,12 +1,12 @@
 function update-groups
-    for group in (get iam/groups)
+    for group in (get groups)
         group-exists $group
         or create-group $group
         and begin
-            for policy in (get iam/groups/$group/policies)
-                apply-policy iam/groups/$group/policies/$policy
+            for policy in (get groups/$group/policies)
+                apply-policy groups/$group/policies/$policy
             end
-            for user in (get iam/groups/$group/users)
+            for user in (get groups/$group/users)
                 add-user-to-group $user $group
             end
         end
