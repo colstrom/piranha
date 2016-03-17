85ca92922cc4d731b724fc72a9293fc869adddff
Chris Olstrom <chris@olstrom.com>
2016-03-17 08:15:28 -0700

    Add usage to print-status

--- functions/print-status.fish
+++ functions/print-status.fish
@@ -1,3 +1,8 @@
 function print-status --argument tag message
+    if empty "$tag" "$message"
+        usage print-status '<tag>' '<message>'
+        return (false)
+    end
+
     printf "[%s] $message\n" (set_color blue)$tag(set_color normal) >& 2
 end
