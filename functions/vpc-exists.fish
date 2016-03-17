084df4dbc2b5fb9beae4e18f015540bc32947fed
Chris Olstrom <chris@olstrom.com>
2016-03-17 08:16:07 -0700

    Add usage to vpc-exists

--- functions/vpc-exists.fish
+++ functions/vpc-exists.fish
@@ -1,4 +1,9 @@
 function vpc-exists --argument name
+    if empty "$name"
+        usage vpc-exists '<name>'
+        return (false)
+    end
+
     print-status vpc/exists $name
     test (count (lookup vpc Name $name)) -gt 0
 end
