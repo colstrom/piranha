5cf7ee66a9fe184f9b5a4f34e2ed2cb1143a3212
Chris Olstrom <chris@olstrom.com>
2016-03-16 22:00:47 -0700

    Prefer --argument to --argument-names

--- functions/elastic-ips.fish
+++ functions/elastic-ips.fish
@@ -1,4 +1,4 @@
-function elastic-ips --argument-names resource
+function elastic-ips --argument resource
     set --local resource_type (echo "$resource" | cut -f 1 -d '-')
     if test "$resource_type" = 'eni'
         aws ec2 describe-network-interfaces --filters Name=network-interface-id,Values=$resource | jq --raw-output .NetworkInterfaces[].Association.PublicIp
