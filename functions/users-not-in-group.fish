9236fe849124e4f94cc9eaac377ccc29da32a948
Chris Olstrom <chris@olstrom.com>
2016-03-17 09:22:20 -0700

    Add function to find users not in a group

---
+++ functions/users-not-in-group.fish
@@ -0,0 +1,8 @@
+function users-not-in-group --argument group
+    if empty "$group"
+        usage users-not-in-group '<group>'
+        return (false)
+    end
+
+    comm -13 (users-in-group $group | psub) (get iam/users | psub)
+end
