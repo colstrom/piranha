8cf195792af7c5d40491502172b78f86dbbde4ae
Chris Olstrom <chris@olstrom.com>
2016-03-16 22:35:11 -0700

    Consistent exit status for usage in add-tag-to-vpc

--- functions/add-tag-to-vpc.fish
+++ functions/add-tag-to-vpc.fish
@@ -2,7 +2,7 @@ function add-tag-to-vpc --argument resource key value
     print-status vpc/add-tag "$argv"
     if empty "$resource" "$key" "$value"
         usage add-tag-to-vpc '<vpc-id>' '<key>' '<value>'
-        return -1
+        return (false)
     end
 
     and aws ec2 create-tags --resources $resource --tags Key=$key,Value=$value
