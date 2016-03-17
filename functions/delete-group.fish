b23811d87fde61bdae300d57ca0c16ee39100811
Chris Olstrom <chris@olstrom.com>
2016-03-17 11:00:11 -0700

    Add function to delete a group

---
+++ functions/delete-group.fish
@@ -0,0 +1,18 @@
+function delete-group --argument group
+    if empty "$group"
+        usage delete-group '<group>'
+        return (false)
+    end
+
+    print-status delete-group "$argv"
+
+    for policy in (attached-policies group "$group")
+        detach-policy group "$group" "$policy"
+    end
+
+    for user in (actual-users-in-group "$group")
+        delete-from-group "$group" "$user"
+    end
+
+    aws iam delete-group --group-name "$group"
+end
