4fa5990c9af268044e5ce0dc24488178ad93bc76
Chris Olstrom <chris@olstrom.com>
2016-03-17 08:14:33 -0700

    Add usage to bucket-exists

--- functions/bucket-exists.fish
+++ functions/bucket-exists.fish
@@ -1,4 +1,9 @@
 function bucket-exists --argument name
+    if empty "$name"
+        usage bucket-exists '<name>'
+        return (false)
+    end
+
     print-status s3/bucket-exists $name
     aws s3api list-buckets | jq -e ".Buckets[] | select(.Name == \"$name\")" > /dev/null ^ /dev/null
 end
