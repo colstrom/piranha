ac0a1ae35eecf0553d0d995c2521c9942f51a311
Chris Olstrom <chris@olstrom.com>
2016-04-20 16:39:22 -0700

    Add print function

---
+++ functions/print.fish
@@ -0,0 +1,41 @@
+function print
+    for arg in $argv
+        if echo $arg | grep -qE '^-+'
+            set opts $opts (echo $arg | sed -E 's/^-+//')
+        else
+            set args $args $arg
+        end
+    end
+
+    set color normal
+
+    for opt in $opts
+        switch $opt
+            case p pass success
+                set color green
+                set state success
+            case f fail failure
+                set color red
+                set state failure
+            case w warn warning
+                set color yellow
+                set state warning
+            case b bold
+                set color_flags '-o'
+            case 't=*' 'tag=*'
+                set tag (echo $opt | awk -F = '{ print $2 }')
+            case '*'
+                set color cyan
+        end
+    end
+
+    if test -z "$tag"
+        set tag $state
+    end
+
+    if test -z "$tag"
+        echo "$args" >& 2
+    else
+        printf "[%s] $args\n" (set_color $color_flags $color)$tag(set_color normal) >& 2
+    end
+end
