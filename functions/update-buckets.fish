2836c49ab4de119e4b2cc109327c2e7f4e30e1a4
Stephen DeGabriele <sdegabriele@bbtv.com>
2016-04-24 16:27:09 -0700

    remove s3 from update buckets

--- functions/update-buckets.fish
+++ functions/update-buckets.fish
@@ -1,9 +1,9 @@
 function update-buckets
-    for bucket in (get s3/buckets)
+    for bucket in (get buckets)
         bucket-exists $bucket
         or create-bucket $bucket
-        and for policy in (get s3/buckets/$bucket/policies)
-            apply-policy s3/buckets/$bucket/policies/$policy
+        and for policy in (get buckets/$bucket/policies)
+            apply-policy buckets/$bucket/policies/$policy
         end
     end
 end
